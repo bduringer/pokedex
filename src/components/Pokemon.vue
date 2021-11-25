1f777670cd54d306accd142b3a9b4c7013a9825a
bduringer <bduringer@yahoo.com.br>
2021-11-25 16:31:19 -0300

    Showing pokemon

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,16 +1,45 @@
 <template>
   <div>
-  pokemon   
+    <div>{{pokemon.name}}</div>
+    
+    <div v-for='(url, angle) in pokemon.sprites'>
+      <img v-if='typeof url == "string"' :src='url'>
+    </div>
+
+    <div>
+      <h2>Habilidades</h2>
+      <div v-for='move in pokemon.moves' :key='move.move.name'>
+        {{move.move.name}}
+      </div>
+    </div>
+
+    <div>
+      <h2>Versões</h2>
+      <div v-for='game in pokemon.game_indices' :key='game.version.name'>
+        {{game.version.name}}
+      </div>
+    </div> 
   </div>
 </template>
 
 <script>
 export default {
 
+  data () {
+    return { 
+      pokemon: {
+        sprites: {},
+        moves: [],
+      }
+    }
+  },
   
   created () {
-
-  }
+    fetch('https://pokeapi.co/api/v2/pokemon/' + this.$route.params.id).then(res => res.json()).then(data => {
+      this.pokemon = data
+      console.log(this.pokemon)
+    })
+  },
 }
 </script>
 
