1b1021453ba7c71af000d9992a38190ad71e5d8f
bduringer <bduringer@yahoo.com.br>
2021-11-29 16:40:14 -0300

    Last Version

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,30 +1,32 @@
 <template>
-  <div>
-    <div>{{pokemon.name}}</div>
+<div class="container">
+  <div class="corpo">
+    <div><h1 class="name">{{pokemon.name}}</h1></div>
     
-    <div v-for='(url, angle) in pokemon.sprites'>
+    <div class="sprites" v-for='(url, angle) in pokemon.sprites' :key='angle'>
       <img v-if='typeof url == "string"' :src='url'>
     </div>
 
     <div>
-      <h2>Habilidades</h2>
-      <div v-for='move in pokemon.moves' :key='move.move.name'>
+      <h2 class="name">Habilidades:</h2>
+      <div class="ability" v-for='move in pokemon.moves' :key='move.move.name'>
         {{move.move.name}}
       </div>
     </div>
 
     <div>
-      <h2>Versões</h2>
-      <div v-for='game in pokemon.game_indices' :key='game.version.name'>
+      <h2 class="name">Aparece nos pokemon's:</h2>
+      <div class="ability" v-for='game in pokemon.game_indices' :key='game.version.name'>
         {{game.version.name}}
       </div>
     </div> 
     <div>
-      <h2>Evoluções</h2>
-      <div v-for='evolution in evolutions'>
-        {{evolution}}
-      </div>
+      <h2 class="name">Evoluções:</h2>
+      <a class="ability evolves" v-for='evolution in evolutions' :key='evolution.name' :href="'/pokemons/' + evolution.name">
+        {{evolution.name}}
+      </a>
     </div> 
+  </div>
   </div>
 </template>
 
@@ -45,13 +47,18 @@ export default {
     getEvolutions () {
       fetch(this.pokemon.species.url).then(res => res.json()).then(species => {
         fetch(species.evolution_chain.url).then(res => res.json()).then(evolutionChain => {
-          this.evolutions = [species.name, ...this.nextEvolutions(evolutionChain.chain)]
+          this.evolutions = this.nextEvolutions(evolutionChain.chain)
         })
       })
     },
-    nextEvolutions (chain)  {
-      return chain.evolves_to.reduce((memo, evolution) => [...memo, evolution.species.name, ...this.nextEvolutions(evolution)], [])
-    }
+    nextEvolutions (evolution) {      
+      if (!evolution.evolves_to) return
+      var nextEvolutions = evolution.evolves_to.reduce((memo, nextEvolution) => {
+        memo.push(...this.nextEvolutions(nextEvolution))
+        return memo
+      }, [])
+      return [evolution.species, ...nextEvolutions]
+    },
   },
   
   created () {
@@ -65,4 +72,56 @@ export default {
 
 <style scoped>
 
+  .evolves{
+    margin-bottom: 30px;
+  }
+  .ability{
+    display: inline-block;
+    text-transform: uppercase;
+    color: black;
+    text-shadow: 1px 1px 2px black, blue, yellow;
+    margin-right: 10px;
+    margin-left: 10px;
+    max-width: 100%;
+  }
+  .name{
+    text-transform: uppercase;
+    color: black;
+    text-shadow: 1px 1px 2px black, blue, yellow;
+    max-width: 100%;
+  }
+  
+  .corpo{
+    display: block;
+    position: relative;
+    margin-top: 30px;
+    background-image: linear-gradient(to right, #00BFFF , #4169E1);
+    border: solid;
+    box-shadow: inset 0 0 1em black, 0 0 1em grey;
+    font-family: Blippo, fantasy;
+    text-align: center;
+    font-size: 30px;
+    margin-left: 20%;
+    margin-right: 20%;
+    border-radius: 50px;
+    max-width: 100%;
+  }
+  .sprites{
+    display: inline-block;
+    max-width: 100%;
+  }
+
+  .sprites img{
+    width: 200px;
+  }
+
+  .container{
+    background-image: url('../assets/slowpoke.png');
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    padding: 40px;
+    max-width: 100%;
+  }
 </style>
--- src/components/Pokemons.vue
+++ src/components/Pokemons.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
-   <!-- <input @change='filterByName'>
+  <div class="container">
+  <div class="body">
+   <!-- Nesta parte tentei botar os inputs para o filtro:
+    <input @change='filterByName'>
     <input @change='filterByType'>
     <input @change='filterById'>
     <input @change='filterBySpecies'>-->
-
-
-    <div v-for='pokemon in filteredPokemons' :key='pokemon.name'>
-      <router-link :to='"/pokemons/"+ pokemon.name'>{{pokemon.name}}</router-link>
-    </div>
-    <div v-infinite-scroll="next" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
-    </div>
+      <img class="logo" src="../assets/Logo_pokedex.png">
+      <div class="pokemons" v-for='pokemon in filteredPokemons' :key='pokemon.name'>
+        <router-link class="pokemons" :to='"/pokemons/"+ pokemon.name'>
+          {{pokemon.name}}
+        </router-link>
+      </div>
+    <div v-infinite-scroll="next" infinite-scroll-disabled="busy" infinite-scroll-distance="10"></div>
+  </div>
   </div>
 </template>
 
@@ -19,8 +22,9 @@ export default {
   computed: {
     filteredPokemons () {
       return this.pokemons.filter(pokemon => {
-        var id = pokemon.url.match(/\/(\d+)/)[1]
+       // var id = pokemon.url.match(/\/(\d+)/)[1]
         return (
+          // Obs: Aqui tentei fazer um filtro, mas não consegui :(
           // pokemon.name.search(this.filters.name) >= 0 &&
           // pokemon.types.reduce((memo, type) => memo || type.type.name.search(this.filters.type) >= 0, false) && 
           // (!this.filters.id || id == this.filters.id)
@@ -38,24 +42,26 @@ export default {
   },
   methods: {
     next () {
+      if (!this.nextUrl) return
+      
       fetch(this.nextUrl).then(res => res.json()).then(data => {
         this.pokemons.push(...data.results)
         this.nextUrl = data.next
       })
     },
-    filterByName (e) {
-      this.filters.name = e.target.value
-    },
+    // filterByName (e) {
+    //   this.filters.name = e.target.value
+    // },
 
-    filterByType (e) {
-      this.filters.type = e.target.value
-    },
-    filterById (e) {
-      this.filters.id = e.target.value
-    },
-    filterBySpecies (e) {
-      this.filters.species = e.target.value
-    }
+    // filterByType (e) {
+    //   this.filters.type = e.target.value
+    // },
+    // filterById (e) {
+    //   this.filters.id = e.target.value
+    // },
+    // filterBySpecies (e) {
+    //   this.filters.species = e.target.value
+    // }
   },
   created () {
     fetch('https://pokeapi.co/api/v2/pokemon/?limit=60').then(res => res.json()).then(data => {
@@ -67,5 +73,45 @@ export default {
 </script>
 
 <style scoped>
+
+  .body {
+    display: block;
+    position: relative;
+    margin-top: 30px;
+    background-image: linear-gradient(to right, yellow, red);
+    border: solid;
+    box-shadow: inset 0 0 1em gold, 0 0 1em red;
+    font-family: Blippo, fantasy;
+    text-align: center;
+    font-size: 50px;
+    margin-left: 20%;
+    margin-right: 20%;
+    border-radius: 50px;
+  }
+  .logo {
+    max-width: 100%;
+    margin-bottom: -50px;
+    margin-top: -30px;
+  }
+  .pokemons{
+    text-transform: uppercase;
+    color: white;
+    text-shadow: 1px 1px 2px black, 0 0 1em blue, 0 0 0.2em yellow;
+    text-decoration: none;
+    margin-bottom: 40px;
+    max-width: 100%;
+  }
+  .container{
+    background-image: url("../assets/bg_pokemon.png");
+    background-repeat: repeat;
+    background-size: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    padding: 40px;
+    max-width: 100%;
+  }
+  
 
 </style>
